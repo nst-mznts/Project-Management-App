464de6f2a5a343b8d4a8109c1e6290f3f90ecf68
Anastasiia Mezentseva <middlenast@gmail.com>
2024-05-30 15:34:22 +0200

    feat: implement a board list

--- src/components/Boards/Boards.jsx
+++ src/components/Boards/Boards.jsx
@@ -2,22 +2,57 @@ import './Boards.scss';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { MdAdd } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 import Sidebar from '../Sidebar/Sidebar';
 import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import ModalWindow from '../ModalWindow/ModalWindow';
 
+let listOfTasks = [
+    {id: 1, title: 'first board', tasks: [{id: 1, title: 'title 1'}, {id: 2, title: 'title 2'}]},
+    {id: 2, title: 'second board', tasks: [{id: 3, title: 'title 3'}]},
+    {id: 3, title: 'third board', tasks: []},
+];
+
 function Boards({ isSidebarOpen, openSidebar, openStartPage, userName }) {
+    const [boards, setBoards] = useState(listOfTasks);
     const [isModalWindowOpened, setIsModalWindowOpened] = useState(false);
     const { t } = useTranslation();
+
+    const addNewBoard = () => {
+        const newBoard = {
+          id: nanoid(),
+          title: t("add-new-board"),
+          tasks: []
+        }
+        const newBoards = [...boards, newBoard];
+        setBoards(newBoards);
+    }
 
     return (
         <main className="boards-page">
             <div className="boards-page-content">
                 <div className='start-button'>
-                    <button className='button rectangular-button colored'>
+                    <button className='button rectangular-button colored' onClick={addNewBoard}>
                         <MdAdd size="2em"/>
                         {t("add-board-button")}
                     </button>
+                </div>
+                <div className="boards-wrapper">
+                    {boards.map(board => {
+                        return (<div key={board.id} className="board">
+                            {board.title}
+                            <div className="boards-buttons-wrapper">
+                                <button className='button round-button additional-colored'>
+                                    <MdEdit size="2em"/>
+                                </button>
+                                <button className='button round-button additional-colored'>
+                                    <MdDelete size="2em"/>
+                                </button>
+                            </div>
+                        </div>)
+                    })}
                 </div>
                 {isModalWindowOpened && (
                     <ModalWindow
